846181b6320d9c908501843fbd8fbf2d91dd19cd
prajithravisankar <prajithravisankar@gmail.com>
2025-12-29 10:19:30 -0500

    customized how models are displayed in the admin site

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,4 +3,15 @@ from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+# how we want to display it in the admin site for blogs page
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
