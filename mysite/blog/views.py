d778ea7d996233c76b22ba2fadbe742bfca1e3cc
prajithravisankar <prajithravisankar@gmail.com>
2025-12-30 06:48:01 -0500

    implemented seo friendly urls based on year/month/day/slug fields, modified urls, then views, then models to achieve seo friendly urls now reflects on the browser

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,11 +8,11 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
     # except:
     #     raise Http404('No post found')
     # return render(request, 'blog/post/detail.html', {'post': post})
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
