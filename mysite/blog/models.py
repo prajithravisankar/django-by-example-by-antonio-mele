7da83b8f8b69d67cac89567293b1ab25f4cbc399
prajithravisankar <prajithravisankar@gmail.com>
2025-12-30 06:35:58 -0500

    altered slugs field in blog model to now only allow unique blog post for a given publish date

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250) # for clean seo friendly urls refer (pg 47/821)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
