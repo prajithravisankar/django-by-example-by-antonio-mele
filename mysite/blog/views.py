6041092f38347a9b79e3d189f0c04f204af7b101
prajithravisankar <prajithravisankar@gmail.com>
2025-12-30 07:27:52 -0500

    added pagination. changed views.py to include django's built in paginator, used it to display 3 blog posts per page, created new pagination.html template and used it in list.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)  # 3 posts per page
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
