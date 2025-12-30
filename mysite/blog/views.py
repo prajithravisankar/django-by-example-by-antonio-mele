40bce253cb4bfddaf0c78d14bfe43d6802d072fd
prajithravisankar <prajithravisankar@gmail.com>
2025-12-30 07:57:20 -0500

    implemented class based views instead of function based views, learned the advantages of using generic built in django templates for building classes.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
+
+class PostListView(ListView):
+    """
+    Alternative post list view using class-based views, alternative to post_list function based view.
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
+
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)  # 3 posts per page
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
